260be34fc602dd277438c76bfbda782d9fe14c39
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-21 11:34:09 +1000

    express route handlers

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+
 // Phase 2
 const {
   getAllArtists,
@@ -25,6 +26,22 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json());""
+
+app.post('/', (req, res) => {
+  res.json({ received: req.body });
+})
+
+app.get('/artists', (req, res) => {
+  const artists = getAllArtists(); // get the data from the data.js function
+  res.status(200).json(artists); // send the data as json with a 200 status
+})
+
+app.post('/artists', (req, res) => {
+  const newArtist = req.body; // access the data sent in the request body
+  const addedArtist = addArtist(newArtist); // add the artist using data.js function
+  res.status(201).json(addedArtist); // respond with the created artist and 201 status
+})
 
 // DO NOT MODIFY
 if (require.main === module) {
